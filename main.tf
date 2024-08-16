dffd0ae85feda9c53c347bf971cc39c767928792
Aksel121 <aksellutfu@gmail.com>
2024-08-16 12:21:15 -0400

    updated

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "network" {
   source = "./modules"
+  
 
-  vpc_cidr_block = "10.0.0.0/16"
 
   sg_rules = [
     {
@@ -16,7 +16,7 @@ module "network" {
       type        = "egress"
       from_port   = 0
       to_port     = 0
-      protocol    = "-1"
+      
       cidr_blocks = ["0.0.0.0/0"]
       description = "All traffic"
     }
--- modules/variables.tf
+++ modules/variables.tf
@@ -1,17 +1,17 @@
 variable "vpc_cidr_block" {
   description = "CIDR block for VPC"
   type        = string
-  default     = "0.0.0.0/16"
+  default     = "10.0.0.0/16"
 }
 
 variable "sg_rules" {
   description = "Security group rules"
   type = list(object({
     type        = string
-    from_port   = optional(number)
-    to_port     = optional(number)
+    from_port   = number
+    to_port     = number
     protocol    = optional(string, "-1")
-    cidr_blocks = optional(list(string), ["0.0.0.0/0"])
+    cidr_blocks = list(string)
     description = optional(string, "")
   }))
   default = []
